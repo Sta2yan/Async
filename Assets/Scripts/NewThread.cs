a7d2de8fe9095fa38dd3c80aba0ce12a25bb8baa
Станислав <s2_stas@mail.ru>
2024-07-27 15:54:14 +0300

    Update NewThread.cs

--- Assets/Scripts/NewThread.cs
+++ Assets/Scripts/NewThread.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class NewThread : MonoBehaviour
@@ -6,14 +7,25 @@ public class NewThread : MonoBehaviour
     private Thread _thread;
     private int _randomNumber;
     
-    public void Execute()
+    public async void Execute()
     {
+        /*#region ДоЛекции
+
         _thread = new Thread(() => _randomNumber = SquareNumber(5));
         _thread.Start();
         
         _thread.Join();
         
         print(_randomNumber);
+
+        #endregion*/
+
+        #region После лекции
+
+        await Task.Run(() => _randomNumber = SquareNumber(5));
+        print(_randomNumber);
+
+        #endregion
     }
 
     private int SquareNumber(int number)
